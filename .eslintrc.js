015d864baf18f716cf3a9abed7d78b2746460264
mshogren <3902009+mshogren@users.noreply.github.com>
2017-01-05 15:39:39 -0700

    Initial commit with icon and eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-console": "off",
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "react/jsx-filename-extension": [
+      "warn",
+      { "extensions": [
+        ".js",
+        ".jsx"]
+      }
+    ],
+  }
+};
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = function App() {
+  return (
+    <div className="App" />
+  );
+};
+
+export default App;
